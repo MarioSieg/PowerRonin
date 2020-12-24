7fda3be87962194dd21e4990f08ea1bbb872ac79
msi <support@kerbogames.com>
2020-12-24 11:16:22 +0100

    Fixes

--- source/renderer/shaders/bumped_diffuse.cpp
+++ source/renderer/shaders/bumped_diffuse.cpp
@@ -7,7 +7,7 @@ namespace dce::renderer::shaders {
 	void BumpedDiffuse::load() {
 		IShader::load();
 		this->u_s_tex_color_ = createUniform("s_tex_color", bgfx::UniformType::Sampler);
-		this->u_s_tex_color_ = createUniform("s_normal_map", bgfx::UniformType::Sampler);
+		this->u_s_normal_map_ = createUniform("s_normal_map", bgfx::UniformType::Sampler);
 		this->u_mat_color_ = createUniform("u_mat_color", bgfx::UniformType::Vec4);
 		this->u_light_dir_ = createUniform("u_light_dir", bgfx::UniformType::Vec4);
 		this->u_ambient_color_ = createUniform("u_ambient_color", bgfx::UniformType::Vec4);
@@ -19,7 +19,7 @@ namespace dce::renderer::shaders {
 		destroy(this->u_ambient_color_);
 		destroy(this->u_light_dir_);
 		destroy(this->u_mat_color_);
-		destroy(this->u_s_normal_map);
+		destroy(this->u_s_normal_map_);
 		destroy(this->u_s_tex_color_);
 		IShader::unload();
 	}
@@ -33,7 +33,7 @@ namespace dce::renderer::shaders {
 	void BumpedDiffuse::per_object(const Mesh& _mesh, const Material::BumpedDiffuse& _mat) const {
 		this->gpu_.set_mesh_buffer(_mesh);
 		this->gpu_.set_texture(_mat.albedo, this->u_s_tex_color_, 0);
-		this->gpu_.set_texture(_mat.normal, this->u_s_normal_map, 1);
+		this->gpu_.set_texture(_mat.normal, this->u_s_normal_map_, 1);
 		this->gpu_.draw(this->program_, SCENERY_VIEW);
 	}
 }
