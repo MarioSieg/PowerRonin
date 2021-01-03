a1d135e6df000d72dcbaf6e9c5feddfcc5a841d7
msi <support@kerbogames.com>
2021-01-03 21:09:45 +0100

    Updates

--- source/scripting/internal_calls.cpp
+++ source/scripting/internal_calls.cpp
@@ -4,7 +4,6 @@
 
 #include "internal_calls.hpp"
 #include "mono_headers.hpp"
-#include "native_config.hpp"
 
 #include <cstdint>
 
@@ -22,43 +21,53 @@ namespace dce::scripting
 		
 		static auto* const RUNTIME = &_rt;
 
-		REGISTER_CALL("ProtocolLog", [](const LogLevel _level, MonoString* const _message) {
+		REGISTER_CALL("ProtocolLog", [](const LogLevel _level, MonoString* const _message) 
+		{
 				auto* const str = mono_string_to_utf8(_message);
 				RUNTIME->scripting_protocol().log(_level, std::string(str));
 				mono_free(str);
 		})
 
-		REGISTER_CALL("ProtocolFlush", []() {
+		REGISTER_CALL("ProtocolFlush", []() 
+		{
 			RUNTIME->scripting_protocol().get_logger()->flush();
 		})
 
-		REGISTER_CALL("ProtocolSetFormatPattern", [](MonoString* const _pattern) {
+		REGISTER_CALL("ProtocolSetFormatPattern", [](MonoString* const _pattern) 
+		{
 			auto* const str = mono_string_to_utf8(_pattern);
-			RUNTIME->scripting_protocol().get_logger()->set_pattern(str); mono_free(str);
+			RUNTIME->scripting_protocol().get_logger()->set_pattern(str);
+			mono_free(str);
 		})
 
-		REGISTER_CALL("InputIsKeyDown", [](const std::int16_t _key) noexcept -> bool {
+		REGISTER_CALL("InputIsKeyDown", [](const std::int16_t _key) noexcept -> bool 
+		{
 			return RUNTIME->input().is_key_down(static_cast<Key>(_key));
 		})
 
-		REGISTER_CALL("InputGetCursorPosition", [](float* const _x, float* const _y) noexcept {
+		REGISTER_CALL("InputGetCursorPosition", [](float* const _x, float* const _y) noexcept 
+		{
 			const auto cursor_pos = RUNTIME->input().mouse_pos();
 			*_x = cursor_pos.x; *_y = cursor_pos.y;
 		})
 
-		REGISTER_CALL("InputIsMouseDown", [](const std::uint16_t _mouse_button) noexcept -> bool {
+		REGISTER_CALL("InputIsMouseDown", [](const std::uint16_t _mouse_button) noexcept -> bool 
+		{
 			return RUNTIME->input().is_mouse_button_down(static_cast<MouseButton>(_mouse_button));
 		})
 
-		REGISTER_CALL("CfgApplyDisplay", [](const DisplayConfig& _cfg) {
+		REGISTER_CALL("CfgApplyDisplay", [](const DisplayConfig& _cfg) 
+		{
 			RUNTIME->config().display = _cfg;
 		})
 
-		REGISTER_CALL("CfgApplyEditor", [](const EditorConfig& _cfg) {
+		REGISTER_CALL("CfgApplyEditor", [](const EditorConfig& _cfg) 
+		{
 			RUNTIME->config().editor = _cfg;
 		})
 
-		REGISTER_CALL("CfgApplyGraphics", [](const GraphicsConfig& _cfg) {
+		REGISTER_CALL("CfgApplyGraphics", [](const GraphicsConfig& _cfg) 
+		{
 			RUNTIME->config().graphics = _cfg;
 		})
 
